5f10b5924c4c1c0e63160546734138acdfb8ef94
Roman Klym <klymroman@protonmail.com>
2025-01-11 21:29:29 -0600

    updated mobile footer to have dex buttons together on the right, made buy button slightly bigger

--- lib/widgets/footer_mobile.dart
+++ lib/widgets/footer_mobile.dart
@@ -36,41 +36,46 @@ class _FooterMobileState extends State<FooterMobile> {
                           mode: LaunchMode.externalApplication);
                     }
                   },
-                  fontSize: 12.0,
-                  iconSize: 14.0,
-                  sizedBoxSize: 16.0,
+                  fontSize: 14.0,
+                  iconSize: 16.0,
+                  sizedBoxSize: 14.0,
                 ),
-                CustomButton(
-                  svgPath: 'assets/dex.svg',
-                  label: 'DexScreener',
-                  color: const Color(0xFFf8c3b6),
-                  onTap: () async {
-                    const url =
-                        "https://dexscreener.com/solana/ED5nyyWEzpPPiWimP8vYm7sD7TD3LAt3Q3gRTWHzPJBY";
+                Row(
+                  children: [
+                    CustomButton(
+                      svgPath: 'assets/dex.svg',
+                      label: 'DexScreener',
+                      color: const Color(0xFFf8c3b6),
+                      onTap: () async {
+                        const url =
+                            "https://dexscreener.com/solana/ED5nyyWEzpPPiWimP8vYm7sD7TD3LAt3Q3gRTWHzPJBY";
 
-                    if (await canLaunchUrl(Uri.parse(url))) {
-                      await launchUrl(Uri.parse(url),
-                          mode: LaunchMode.externalApplication);
-                    }
-                  },
-                  fontSize: 10.0,
-                  iconSize: 12.0,
-                ),
-                CustomButton(
-                  svgPath: 'assets/dextools.svg',
-                  label: 'DexTools',
-                  color: const Color(0xFF666A75),
-                  onTap: () async {
-                    const url =
-                        "https://dexscreener.com/solana/ED5nyyWEzpPPiWimP8vYm7sD7TD3LAt3Q3gRTWHzPJBY";
+                        if (await canLaunchUrl(Uri.parse(url))) {
+                          await launchUrl(Uri.parse(url),
+                              mode: LaunchMode.externalApplication);
+                        }
+                      },
+                      fontSize: 10.0,
+                      iconSize: 12.0,
+                    ),
+                    SizedBox(width: 8.0),
+                    CustomButton(
+                      svgPath: 'assets/dextools.svg',
+                      label: 'DexTools',
+                      color: const Color(0xFF666A75),
+                      onTap: () async {
+                        const url =
+                            "https://dexscreener.com/solana/ED5nyyWEzpPPiWimP8vYm7sD7TD3LAt3Q3gRTWHzPJBY";
 
-                    if (await canLaunchUrl(Uri.parse(url))) {
-                      await launchUrl(Uri.parse(url),
-                          mode: LaunchMode.externalApplication);
-                    }
-                  },
-                  fontSize: 10.0,
-                  iconSize: 12.0,
+                        if (await canLaunchUrl(Uri.parse(url))) {
+                          await launchUrl(Uri.parse(url),
+                              mode: LaunchMode.externalApplication);
+                        }
+                      },
+                      fontSize: 10.0,
+                      iconSize: 12.0,
+                    ),
+                  ],
                 ),
               ],
             )
